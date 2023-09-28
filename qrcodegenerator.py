1373461af20739fd4a1af2a5973ab1c04c9f8957
Ömer Topal <omertopal7874@gmail.com>
2023-09-28 11:44:15 +0300

    'qrcodegenerator.py' added.

---
+++ qrcodegenerator.py
@@ -0,0 +1,25 @@
+import qrcode
+import time
+
+Oluşturulacak_Metin = input("Oluşturulacak Metini Giriniz('q' ile durdurabilirsiniz):")
+
+path = "C:/Users\Ömer\Desktop/qrcode.png"
+
+while True:
+
+    if Oluşturulacak_Metin.lower() == "q":
+        print("Qr Kod Oluşturma Durduruldu")
+        time.sleep(3)
+        break
+    else:
+        print("Program Çalışmaya Başladı Lütfen Bekleyiniz")
+    try:
+        img = qrcode.make(Oluşturulacak_Metin)
+        img.save(path)
+        print("İçerik Kaydedildi")
+    finally:
+        print("Program Sonlandırıldı")
+        time.sleep(3)
+        break
+
+print("This program is created omertopal")
